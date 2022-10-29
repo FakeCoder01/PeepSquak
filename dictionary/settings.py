25467cc2d43b06427c4238f37c790cb7bc62352f
FakeCoder01 <ayonsamajder@gmail.com>
2022-10-30 01:22:26 +0700

    v1.0.1

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1jhilm_c55he7)24_)y503ng_*!#f#&0v^olw5o1h60x8*ce9)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
